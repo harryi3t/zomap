b5e829db5682f58f8e33c49e1fff4c90a29a75be
harryi3t <66hari@gmail.com>
2017-01-21 01:22:51 +0530

    searches for restaurants and shows marker

--- src/js/app.js
+++ src/js/app.js
@@ -3,10 +3,13 @@ var sidebarHidden = false;
 var marker;
 var input;
 var autocomplete;
-
+var zomatoAPI = 'https://developers.zomato.com/api/v2.1';
 
 function initMap() {
-  var btm = {lat: 12.9135919, lng: 77.6122421};
+  var btm = {
+    lat: 12.9135919,
+    lng: 77.6122421
+  };
   map = new google.maps.Map(document.getElementById('map'), {
     zoom: 14,
     center: btm
@@ -48,14 +51,60 @@ function place_changed() {
     map.fitBounds(place.geometry.viewport);
   } else {
     map.setCenter(place.geometry.location);
-    map.setZoom(17);  // Why 17? Because it looks good.
+    map.setZoom(17); // Why 17? Because it looks good.
   }
   marker.setPosition(place.geometry.location);
   marker.setVisible(true);
-  console.log(place);
+  getRestaurants(place);
 
   // infowindowContent.children['place-icon'].src = place.icon;
   // infowindowContent.children['place-name'].textContent = place.name;
   // infowindowContent.children['place-address'].textContent = address;
   // infowindow.open(map, marker);
 }
+
+function getRestaurants(place) {
+  if (!place.geometry) return;
+
+  var lat = place.geometry.location.lat();
+  var lon = place.geometry.location.lng();
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.readyState == XMLHttpRequest.DONE
+      && xmlhttp.status == 200) {
+        var data;
+        try {
+          data = JSON.parse(xmlhttp.responseText);
+        } catch(err) {
+          console.error(err);
+          return;
+        }
+        showRestaurants(data.restaurants);
+    }
+  };
+
+  xmlhttp.open('GET', zomatoAPI + '/search?lat='+lat+'&lon='+lon, true);
+  xmlhttp.send();
+}
+
+function showRestaurants(restaurants) {
+  restaurants.forEach(
+    function (obj) {
+      var res = obj.restaurant;
+      res.marker = new google.maps.Marker({
+        position: {
+          lat: parseFloat(res.location.latitude),
+          lng: parseFloat(res.location.longitude)
+        },
+        map: map
+      });
+    }
+  );
+}
+
+XMLHttpRequest.prototype.realSend = XMLHttpRequest.prototype.send;
+var newSend = function(vData) {
+  this.setRequestHeader('user-key', 'b78d1ebc7a9795b00f1f4d04a2fbf8ef');
+  this.realSend(vData);
+};
+XMLHttpRequest.prototype.send = newSend;
